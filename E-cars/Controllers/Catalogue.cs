20a32880e559be2f3b84ae0d16b50c9f0f77bbea
SbZhechev <sbzhechev@gmail.com>
2020-12-19 19:55:55 +0200

    Initial commit

---
+++ E-cars/Controllers/Catalogue.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using static E_cars.Controllers.DBManipulator;
+using E_cars.Models;
+
+namespace E_cars.Controllers
+{
+    public class Catalogue : Controller
+    {
+        public IActionResult Index()
+        {
+            Car car = new Car();
+            car.Picture = "/mercedes-cls.jpg";
+            car.Brand = "Mercedes";
+            car.Model = "CLS 500";
+            car.Production_Year = 2018;
+            car.Mileage = 10;
+            car.Price = 50000;
+
+            Car car2 = new Car();
+            car2.Picture = "/mercedes-cls.jpg";
+            car2.Brand = "BMW";
+            car2.Model = "E46";
+            car2.Production_Year = 1998;
+            car2.Mileage = 123456;
+            car2.Price = 8000;
+
+            List<Car> myCars = new List<Car>();
+            myCars.Add(car);
+            myCars.Add(car2);
+
+            return View(myCars);
+        }
+    }
+}
---
+++ E-cars/Models/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_cars.Models
+{
+    public class Car
+    {
+        public string Picture { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Production_Year { get; set; }
+        public int Mileage { get; set; }
+        public int Price { get; set; }
+    }
+}

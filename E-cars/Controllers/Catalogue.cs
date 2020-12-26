c370b4342a95463407a7d2e843b0939133d719ff
Sebastiyan Kasurov <sebastiyan.kasarov@trading.com>
2020-12-26 23:57:47 +0200

    database connection

--- E-cars/Controllers/Catalogue.cs
+++ E-cars/Controllers/Catalogue.cs
@@ -1,38 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using static E_cars.Controllers.DBManipulator;
 using E_cars.Models;
 
 namespace E_cars.Controllers
 {
     public class Catalogue : Controller
     {
-        public IActionResult Index()
+        CatalogueRepository catalogueRepository = new CatalogueRepository();
+
+        public async Task<IActionResult> IndexAsync()
         {
-            Car car = new Car();
-            car.Picture = "/mercedes-cls.jpg";
-            car.Brand = "Mercedes";
-            car.Model = "CLS 500";
-            car.Production_Year = 2018;
-            car.Mileage = 10;
-            car.Price = 50000;
 
-            Car car2 = new Car();
-            car2.Picture = "/mercedes-cls.jpg";
-            car2.Brand = "BMW";
-            car2.Model = "E46";
-            car2.Production_Year = 1998;
-            car2.Mileage = 123456;
-            car2.Price = 8000;
+            List<Car> cars = await catalogueRepository.GetAllAsync();
 
-            List<Car> myCars = new List<Car>();
-            myCars.Add(car);
-            myCars.Add(car2);
-
-            return View(myCars);
+            return View(cars);
         }
     }
 }
---
+++ E-cars/Controllers/CatalogueRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using E_cars.Models;
+using MySqlConnector;
+
+namespace E_cars.Controllers
+{
+    public class CatalogueRepository
+    {
+        MySqlConnection connection;
+
+        public CatalogueRepository()
+        {
+            connection = new MySqlConnection("server=localhost;user=root;password=root;database=e_cars");
+        }
+
+        public async System.Threading.Tasks.Task<List<Car>> GetAllAsync()
+        {
+            await connection.OpenAsync();
+
+            using var command = new MySqlCommand("SELECT id, brand, model, production_year, price, milage, picture_path FROM cars;", connection);
+
+            using var reader = await command.ExecuteReaderAsync();
+
+            List<Car> result = new List<Car>();
+            while (await reader.ReadAsync())
+            {
+                result.Add(BuildCarEntity(reader));
+            }
+            await connection.CloseAsync();
+
+            return result;
+        }
+
+        private Car BuildCarEntity(MySqlDataReader reader)
+        {
+            Car car = new Car
+            {
+                Id = reader.GetInt16(0),
+                Brand = reader.GetString(1),
+                Model = reader.GetString(2),
+                Production_Year = reader.GetInt16(3),
+                Price = reader.GetDouble(4),
+                Mileage = reader.GetDouble(5),
+                Picture = reader.GetString(6)
+            };
+
+            return car;
+        }
+    }
+
+
+}
